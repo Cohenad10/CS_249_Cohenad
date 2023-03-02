8cdbacee0acbdfd3f515828834dcd45821f5b330
Alec Cohen <cohenad@sunypoly.edu>
2023-03-02 16:31:06 -0500

    assign 3 done

---
+++ assign03/src/main/java/edu/cohenad/assign03/LetterPrinter.java
@@ -0,0 +1,29 @@
+package edu.cohenad.assign03;
+import java.util.Scanner;
+
+public class LetterPrinter {
+    public static Letterhead askForLetterheadData(Scanner input) {
+        System.out.println("Enter business name:");
+        String name = input.nextLine();
+
+        System.out.println("Enter boundary character:");
+        char boundaryChar = input.nextLine().charAt(0);
+
+        System.out.println("Enter slogan (max 4 lines):");
+        String[] sloganLines = new String[4];
+        for (int i = 0; i < 4; i++) {
+            sloganLines[i] = input.nextLine();
+        }
+
+        return new Letterhead(name, sloganLines, boundaryChar);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Letterhead header = askForLetterheadData(scanner);
+
+        System.out.println("Your new letterhead:");
+        System.out.println(header);
+    }
+}
+
