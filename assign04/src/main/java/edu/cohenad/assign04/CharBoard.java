4aa175f9c77e8c4ea20731936aea6668b0ef36a5
Alec Cohen <cohenad@sunypoly.edu>
2023-03-09 17:30:43 -0500

    assign 4 done

---
+++ assign04/src/main/java/edu/cohenad/assign04/CharBoard.java
@@ -0,0 +1,76 @@
+package edu.cohenad.assign04;
+
+public class CharBoard {
+    private int rowCnt;
+    private int colCnt;
+    private char fillChar;
+    private char[][] board;
+
+    public CharBoard(int rowCnt, int colCnt, char fillChar) {
+        this.rowCnt = rowCnt;
+        this.colCnt = colCnt;
+        this.fillChar = fillChar;
+        this.board = new char[rowCnt][colCnt];
+
+        for (int row = 0; row < rowCnt; row++) {
+            for (int col = 0; col < colCnt; col++) {
+                board[row][col] = fillChar;
+            }
+        }
+    }
+
+    public void clear() {
+        for (int row = 0; row < rowCnt; row++) {
+            for (int col = 0; col < colCnt; col++) {
+                board[row][col] = fillChar;
+            }
+        }
+    }
+
+    public int getRowCnt() {
+        return rowCnt;
+    }
+
+    public int getColCnt() {
+        return colCnt;
+    }
+
+    public boolean isValidPosition(int row, int col) {
+        return row >= 0 && row < rowCnt && col >= 0 && col < colCnt;
+    }
+
+    public char getPos(int row, int col) {
+        if (isValidPosition(row, col)) {
+            return board[row][col];
+        } else {
+            return ' ';
+        }
+    }
+
+    public boolean setPos(int row, int col, char c) {
+        if (isValidPosition(row, col)) {
+            board[row][col] = c;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String toString() {
+        return rowCnt + " x " + colCnt + " edu.cohenad.assign04.CharBoard (default: " + fillChar + ")";
+    }
+// was failing 1 test when i didnt have edu.cohenad.assign04 and said that was the only error so i added it
+    public String getMapString() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int row = 0; row < rowCnt; row++) {
+            for (int col = 0; col < colCnt; col++) {
+                sb.append(board[row][col]);
+            }
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+}
+
---
+++ exercises03/src/main/java/edu/Cohenad/exercises03/FoodRationer.java
@@ -0,0 +1,51 @@
+package edu.Cohenad.exercises03;
+import java.util.*;
+
+public class FoodRationer {
+    public static void main(String [] args) {
+        Scanner input = new Scanner(System.in);
+        Supplies supplies = new Supplies();
+        System.out.println("Welcome to the trail!");
+
+        System.out.println("Enter total food:");
+        supplies.addFood(input.nextDouble());
+        System.out.println("Enter number of people:");
+        int partyCnt = input.nextInt();
+        double rations = 10;
+
+        double daysLeft = supplies.getTotalFood()/(partyCnt*rations);
+        System.out.println("DAYS LEFT: " + daysLeft);
+/*
+        double y = 2.0;
+        int x = 10;
+        x = (int)(x/y);
+
+        String t = null;
+
+        if(t != null) {
+            System.out.println(t);
+            System.out.println(t.length());
+        }
+
+        String s = "";
+        System.out.println(s);
+        System.out.println(s.length());
+
+        char c = s.charAt(0);
+*/
+        Random r1 = new Random( 42);
+        Random r2 = new Random( 42);
+        System.out.println("RANDOM:");
+        for(int i = 0; i < 5; i ++)
+        {
+            System.out.println(r1.nextDouble());
+            System.out.println(r2.nextDouble());
+        }
+        boolean foundTreasure = true;
+        if(foundTreasure)
+        {
+            System.out.println("Huzzah");
+        }
+
+    }
+}
